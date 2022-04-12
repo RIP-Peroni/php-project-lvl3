1fe4369aa71b947ed1625482af8d32f0ca1f3e37
RIP-Peroni <epih96@mail.ru>
2022-04-12 23:36:34 +0300

    welcome view added

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+{{--    <link href="/css/app.css" rel="stylesheet">--}}
+</head>
+<body class="min-vh-100 d-flex flex-column">
+{{--    шапка--}}
+<header class="flex-shrink-0">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Анализатор страниц</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Главная</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Сайты</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+{{--    <script src="/js/app.js"></script>--}}
+</header>
+
+{{--    контент--}}
+<main class="flex-grow-1">
+    @yield('content')
+</main>
+{{--    подвал--}}
+<footer class="border-top py-3 mt-5 flex-shrink-0">
+    <div class="container-lg">
+        <div class="text-center">
+            <a href="https://github.com/RIP-Peroni" target="_blank">ripperoni</a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
