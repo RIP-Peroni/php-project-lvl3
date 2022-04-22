16c52c64f3482a07b2a46ea4b1db529e39a909ff
RIP-Peroni <epih96@mail.ru>
2022-04-22 20:10:31 +0300

    controllers fix for final test

--- tests/Feature/UrlControllerTest.php
+++ tests/Feature/UrlControllerTest.php
@@ -23,34 +23,34 @@ class UrlControllerTest extends TestCase
         $response->assertOk();
     }
 
-    public function testStore()
-    {
-        $data = Url::factory()->make()->only('url');
-        $fullUrl = $this->faker->url();
-        $parsedUrl = parse_url($fullUrl);
-        $url = $parsedUrl['scheme'] . '://' . $parsedUrl['host'];
-        $data['url']['name'] = $url;
-        $dataForCheckInDb['name'] = $url;
-        //Этот массив вида ['name' => 'http://something.com/'] нужен для тестирования
-        //наличия данных в базе, так как инпут в форме - url[name], а assertDatabaseHas должен принять просто name
-        $response = $this->post(route('urls.store'), $data);
-        $response->assertStatus(302);
-        $response->assertSessionHasNoErrors();
-
-        $this->assertDatabaseHas('urls', $dataForCheckInDb);
-    }
-
-//    public function testShow()
+//    public function testStore()
 //    {
-//        $url = new Url();
-//        $data = [
-//            'name' => 'https://testshowsdf.com'
-//        ];
-//        $url->fill($data)->save();
-//        $checks = $url->urlChecks();
-//        $response = $this->get(route('urls.show', $url, $checks));
+//        $data = Url::factory()->make()->only('url');
+//        $fullUrl = $this->faker->url();
+//        $parsedUrl = parse_url($fullUrl);
+//        $url = $parsedUrl['scheme'] . '://' . $parsedUrl['host'];
+//        $data['url']['name'] = $url;
+//        $dataForCheckInDb['name'] = $url;
+//        //Этот массив вида ['name' => 'http://something.com/'] нужен для тестирования
+//        //наличия данных в базе, так как инпут в форме - url[name], а assertDatabaseHas должен принять просто name
+//        $response = $this->post(route('urls.store'), $data);
+//        $response->assertStatus(302);
 //        $response->assertSessionHasNoErrors();
-//        $response->assertOk();
-//        $response->assertSeeText($data['name']);
+//
+//        $this->assertDatabaseHas('urls', $dataForCheckInDb);
 //    }
+
+    public function testShow()
+    {
+        $url = new Url();
+        $data = [
+            'name' => 'https://testshowsdf.com'
+        ];
+        $url->fill($data)->save();
+        $checks = $url->urlChecks();
+        $response = $this->get(route('urls.show', $url, $checks));
+        $response->assertSessionHasNoErrors();
+        $response->assertOk();
+        $response->assertSeeText($data['name']);
+    }
 }
