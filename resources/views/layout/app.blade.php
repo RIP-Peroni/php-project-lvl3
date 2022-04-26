9c506d2f3d52802da666161fac1eb0c2fd514456
RIP-Peroni <epih96@mail.ru>
2022-04-26 22:29:06 +0300

    css fix

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,24 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-{{--    todo - добавить в сборку в app.css--}}
-{{--    <style>--}}
-{{--        .pagination > li > a,--}}
-{{--        .pagination > li > span {--}}
-{{--            color: black;--}}
-{{--        }--}}
-
-{{--        .pagination > .active > a,--}}
-{{--        .pagination > .active > a:focus,--}}
-{{--        .pagination > .active > a:hover,--}}
-{{--        .pagination > .active > span,--}}
-{{--        .pagination > .active > span:focus,--}}
-{{--        .pagination > .active > span:hover {--}}
-{{--            background-color: black;--}}
-{{--            border-color: black;--}}
-{{--        }--}}
-{{--    </style>--}}
-
     <title>@yield('title')</title>
 
     <script src="{{ asset('js/app.js') }}" defer></script>
